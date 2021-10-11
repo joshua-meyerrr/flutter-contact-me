5a1cd0cc268da0ea82bc62d99bffe92dee39a28c
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-11 16:40:34 +0200

    ♻️ create constants file

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const bgPrimary = Color(0xFF1D2125);
+const bgSecondary = Color(0xFF22282C);
+const accentPrimary = Color(0xFFF0B28E);
+const accentSecondary = Color(0xFFA2ABFF);
